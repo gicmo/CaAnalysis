40a3a89400406a319d2b0d48c056303ade893913
Christian Kellner <christian@kellner.me>
2016-04-14 00:24:14 +0200

    ca-cmp2: compare ephys vs ephys cs calcium

---
+++ ca-cmp2.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import os
+import argparse
+
+import datetime
+import numpy as np
+import pandas as pd
+from scipy import stats
+
+import matplotlib.pyplot as plt
+
+from ca.nix import *
+from ca.img import *
+
+pulses = [1, 3, 10, 25]
+
+
+def main():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('--style', nargs='*', type=str, default=['ck'])
+    parser.add_argument("megatable")
+    parser.add_argument("imaging")
+    parser.add_argument('x', type=str)
+    parser.add_argument('y', type=str)
+    parser.add_argument("--pulse", choices=map(lambda p: 'ap'+str(p), pulses), default='ap25')
+    parser.add_argument("--age", default=None)
+    args = parser.parse_args()
+
+    plt.style.use(args.style)
+
+    mega = pd.read_csv(args.megatable)
+    imga = pd.read_csv(args.imaging)
+
+    mega.set_index(['Neuron'])
+    imga.set_index(['Neuron'])
+
+    result = pd.merge(mega, imga, how='inner', on=['Neuron'])
+    result.reset_index(inplace=True)
+
+    if args.age is not None:
+        result = result.loc[result['Age_x'] == int(args.age)]
+
+    plt.scatter(result[args.x], result[args.y], c=result[args.pulse], cmap='plasma')
+    plt.xlabel(args.x)
+    plt.ylabel(args.y)
+    plt.colorbar()
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
+
