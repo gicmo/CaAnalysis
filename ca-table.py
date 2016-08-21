6c8f3fc9d134135247f18b28e6c84491583ec7c0
Christian Kellner <christian@kellner.me>
2016-08-21 22:59:59 +0200

    ca-table: write gigatable (and default to file)

--- ca-table.py
+++ ca-table.py
@@ -8,12 +8,13 @@ from __future__ import division
 import os
 import argparse
 
-import datetime
 import nixio as nix
 import numpy as np
+import pandas as pd
 
-import matplotlib.pyplot as plt
 import sys
+
+import StringIO
 
 from ca.nix import *
 from ca.img import *
@@ -23,6 +24,7 @@ ages = [10, 11, 13, 14, 15, 16, 17, 18, 60]
 
 
 def plot_peaks(args, nf):
+    import matplotlib.pyplot as plt
     b = item_of_type(nf.blocks, "dff.peak")
     tags = items_of_type(b.multi_tags, "pulse.avg@age")
     tags = filter(lambda x: x.name.endswith(args.pulse), tags)
@@ -60,6 +62,8 @@ def index_of_name(lst, name):
 
 def main():
     parser = argparse.ArgumentParser(description="")
+    parser.add_argument('--stdout', action="store_true", default=False)
+    parser.add_argument("megatable")
     parser.add_argument("file")
 
     args = parser.parse_args()
@@ -76,14 +80,26 @@ def main():
     fage = index_of_name(tag.features, "age")
 
     params = nf.sections['params']
+    baseline = params['baseline']
     over = params['over']
     dlen = params['length']
+    bg = params['bg-correction'] if 'bg-correction' in params else None
 
     # print("neuron feature @ %d " % fneu, file=sys.stderr)
     # print("condition feature @ %d " % fcnd, file=sys.stderr)
     # print("age feature @ %d " % fage, file=sys.stderr)
 
-    print('Neuron,Age,Condition,Over,Length,%s,%s,%s,%s' % tuple(map(lambda t: t.name.split('.')[0], tag.references)))
+    print("baseline: %s" % baseline, file=sys.stderr)
+    print("over correction: %s" % over, file=sys.stderr)
+    print("dendrite length: %s" % dlen, file=sys.stderr)
+    print("background correction: %s" % (bg or 'uncorrected'), file=sys.stderr)
+
+    outfile = StringIO.StringIO('')
+
+    print('Neuron,Age,Condition,Over,Length,%s,%s,%s,%s'
+          % tuple(map(lambda t: t.name.split('.')[0], tag.references)),
+          file=outfile)
+
     for p in pos:
         p = int(p)
         nid = tag.retrieve_feature_data(p, fneu)[0]
@@ -91,10 +107,26 @@ def main():
         cid = tag.retrieve_feature_data(p, fcnd)[0]
         condition = ['control', 'noisebox'][cid]
         age = tag.retrieve_feature_data(p, fage)[0]
-        print('%s,%d,%s,%s,%s,' % (neuron, age, condition, over, dlen), end='')
+        print('%s,%d,%s,%s,%s,' % (neuron, age, condition, over, dlen), end='', file=outfile)
         data = [tag.retrieve_data(p, idx)[0] for idx, _ in enumerate(pulses)]
-        print(",".join(map(str, data)))
+        print(",".join(map(str, data)), file=outfile)
 
+    # Now we construct the giga table
+    outfile.seek(0)
+
+    imga = pd.read_csv(outfile)
+    mega = pd.read_csv(args.megatable)
+
+    result = pd.merge(mega, imga, how='inner', on=['Neuron'])
+    result.reset_index(inplace=True)
+
+    rename = {c: c[:-2] for c in result.columns if c.endswith("_x")}
+    result.rename(columns=rename, inplace=True)
+
+    filename = "img.%s.%s.%s.%s.csv" % (over, baseline, dlen, (bg or 'uc'))
+    result.to_csv(filename if not args.stdout else sys.stdout, index=False)
+    if not args.stdout:
+        print("\ndone, data written to:\n%s\n" % filename)
 
 if __name__ == "__main__":
     main()
